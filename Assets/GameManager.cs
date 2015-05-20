b9986bca1d39d181aa2b4d9818d376318b1c05e6
Krenuds <donthefringe@gmail.com>
2015-05-20 15:03:17 -0400

    Uploaded main assets

---
+++ Assets/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	int sceneId = 0;
+
+	// Use this for initialization
+	void Start () {
+			SetupLevel ();
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void SetupLevel()
+	{
+		GameObject floor = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		Renderer rend = floor.gameObject.GetComponent<Renderer> ();
+		rend.material.color = Color.red;
+		floor.name = GetSceneID("Floor");
+		floor.transform.localScale = new Vector3 (10, .5f, 10);
+		floor.transform.position = new Vector3 (0, -.5f, 0);
+
+	}
+
+	private string GetSceneID(string item)
+	{
+		return "_" + sceneId + " " + item;
+		sceneId ++;
+	}
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # UnityNoEditor
-Here's a commit
