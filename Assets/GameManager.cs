2dfd98128eaca6fcf20e24e9fc96eaffdcf7939d
Krenuds <donthefringe@gmail.com>
2015-06-03 12:03:07 -0400

    Next Gen

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,14 +4,14 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 	int sceneId = 0;
 	Room myRoom;
-	
+	public PhysicMaterial x;	
 
 	// Use this for initialization
 	void Start () {
-		myRoom = new Room (10, 10, 10, Vector3.zero);
-			SetupLevel ();
-		myRoom.DisplayRoomInfo ();
-	    
+		//myRoom = new Room (10, 10, 10, Vector3.zero);
+		//myRoom.DisplayRoomInfo ();
+		SetupLevel ();
+		CreateBall ();
 	}
 	
 	// Update is called once per frame
@@ -28,6 +28,16 @@ public class GameManager : MonoBehaviour {
 		floor.transform.localScale = new Vector3 (10, .5f, 10);
 		floor.transform.position = new Vector3 (0, -.5f, 0);
 
+	}
+
+	private void CreateBall (){
+		GameObject Ball = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+		Ball.name = GetSceneID("Ball");
+		Ball.transform.position = new Vector3 (0, 10f, 0);
+		Ball.AddComponent<Rigidbody> ();
+		Ball.GetComponent<Collider> ().material = x;
+	
+	
 	}
 
 	private string GetSceneID(string item)
